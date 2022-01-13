150f130a03dd0453a11ac5641beeafc1f8626c3c
George Desktop <georgecaws@gmail.com>
2022-01-13 23:58:10 +0000

    Added everything to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,31 +3,36 @@
         color="#1D382F"
         class="white--text"
     >
-        This is a footer
-        <v-btn
-            v-for="item in items"
-            :key="item"
-            class="mx-4 white--text"
-            :href="item.href"
-            icon
-        >
-            <v-icon size=24>{{ item.icon }}</v-icon>
-        </v-btn>
+        <v-row>
+            <v-col>
+                <v-card-text class="text-left">
+                    <v-btn
+                        class="mx-4 white--text text-right"
+                        href="https://github.com/unwrapped-spotify"
+                        icon
+                    >
+                        <v-icon size=24>mdi-coffee</v-icon>
+                    </v-btn>
+                    Buy us a coffee!
+                </v-card-text>
+            </v-col>
+            <v-col justify="center">
+                <v-card-text class="text-center">
+                    Unwrapped Spotify
+                </v-card-text>
+            </v-col>
+            <v-col>
+                <v-card-text class="text-right">
+                    Check out the source code!
+                    <v-btn
+                        class="mx-4 white--text text-right"
+                        href="https://github.com/unwrapped-spotify"
+                        icon
+                    >
+                        <v-icon size=24>mdi-github</v-icon>
+                    </v-btn>
+                </v-card-text>
+            </v-col>
+        </v-row>
     </v-footer>
 </template>
-
-<script>
-    export default {
-        data: () => ({
-            icons: [
-                'mdi-github',
-            ],
-            items: [
-                {
-                    icon: 'mdi-github',
-                    href: 'https://github.com/unwrapped-spotify'
-                }
-            ]
-        }),
-    }
-</script>
