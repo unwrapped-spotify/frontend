5ba0501b6a1b6508b24bc101b0f411d22999896b
George Desktop <georgecaws@gmail.com>
2022-01-15 10:44:13 +0000

    Added email text field and submit/back buttons

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -7,17 +7,56 @@
     >
       <v-col cols="2"></v-col>
       <v-col cols="8">
-        <h1>Enter your email</h1>
-        <v-text-field
-          dark
-          v-model="email"
-          label="Email"
-          :rules="emailRules"
-        ></v-text-field>
-        <p>
-          We only use this to link your data to you, we won't email you unless
-          you request it!
-        </p>
+        <v-row
+          justify="center"
+          class="text-center white--text"
+          align-content="center"
+        >
+          <v-col>
+            <h1>Enter your email</h1>
+            <br />
+            <v-row>
+              <v-col cols="3" />
+              <v-col cols="6">
+                <v-text-field
+                  dark
+                  v-model="email"
+                  label="Email"
+                  :rules="emailRules"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="3" />
+            </v-row>
+            <p>
+              We only use this to link your data to you, we won't email you
+              unless you request it!
+            </p>
+          </v-col>
+        </v-row>
+        <v-row
+          justify="center"
+          class="text-center white--text"
+          align-content="center"
+        >
+          <v-col cols="6">
+            <v-btn
+              rounded
+              :color="$vuetify.theme.themes.dark.primary"
+              @click="changePage('Welcome')"
+            >
+              Back
+            </v-btn>
+          </v-col>
+          <v-col cols="6">
+            <v-btn
+              rounded
+              :color="$vuetify.theme.themes.dark.primary"
+              @click="changePage('Welcome')"
+            >
+              Submit
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col cols="2"></v-col>
     </v-row>
@@ -25,22 +64,26 @@
 </template>
 
 <script>
-  export default{
+  export default {
     data() {
-      return{
-        email: '',
-        emailRules: []
-      }
+      return {
+        email: "",
+        emailRules: [],
+      };
     },
     watch: {
-      email: function(mail) {
-        if (mail != '') {
-        this.emailRules = [v => (v.match(/\S+@\S+\.\S+/)) || 'Invalid email']
-      } else {
-        this.emailRules = ['Email is required']
-      }
-
-      }
-    }
-  }
+      email: function (mail) {
+        if (mail != "") {
+          this.emailRules = [(v) => v.match(/\S+@\S+\.\S+/) || "Invalid email"];
+        } else {
+          this.emailRules = ["Email is required"];
+        }
+      },
+    },
+    methods: {
+      changePage(page) {
+        this.$emit("page", page);
+      },
+    },
+  };
 </script>
