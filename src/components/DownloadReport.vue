df43e8c042120526b1bc1cd360416197c18831bc
George Desktop <georgecaws@gmail.com>
2022-01-20 09:38:48 +0000

    Polls until the report has finished

--- src/components/DownloadReport.vue
+++ src/components/DownloadReport.vue
@@ -1,46 +1,52 @@
 <template>
-  <v-container> 
-    <div class="text-center white--text">{{this.status}}</div>
+  <v-container>
+    <div class="text-center white--text">{{ this.status }}</div>
+    <div class="text-center white--text">{{ this.downloadLink }}</div>
   </v-container>
 </template>
 
 <script>
-export default {
-  props: ["buildID", "storageID"],
-  data() {
-    return {
-      downloadLink: "",
-      status: "",
-    };
-  },
-  methods: {
-    printBuildID() {
-      console.log(this.buildID);
+  export default {
+    props: ["buildID", "storageID"],
+    data() {
+      return {
+        downloadLink: "",
+        status: "",
+      };
     },
-    getStatus: function () {
-      this.axios
-        .get(
-          process.env.VUE_APP_BACKEND_URL +
-          "/api/v1/report/" +
-          this.storageID +
-          "/status?buildID=" +
-          this.buildID)
-        .then((response) => {
-          this.status = response.data.status;
-          //if (response.data.status == "finished") {
-          //  this.downloadLink = "done";
-          //}
-        });
-      setTimeout(this.getStatus, 1000)
-    }
-  },
-  mounted: function() {
-    this.getStatus();
-    //var getStatusPoll = function() {
-    //  this.getStatus;
-    //  setTimeout(getStatusPoll, 1000);
-    //};
-    setTimeout(this.getStatus, 1000)
-  },
-};
+    methods: {
+      printBuildID() {
+        console.log(this.buildID);
+      },
+      getStatus: function () {
+        this.axios
+          .get(
+            process.env.VUE_APP_BACKEND_URL +
+              "/api/v1/report/" +
+              this.storageID +
+              "/status?buildID=" +
+              this.buildID
+          )
+          .then((response) => {
+            this.status = response.data.status;
+            console.log(response.data.status);
+            console.log(response.data.url)
+            if (response.data.status == "SUCCESS") {
+              console.log("Success")
+              // get download link
+            } else {
+              setTimeout(this.getStatus, 1000);
+            }
+          });
+      },
+    },
+    mounted: function () {
+      this.getStatus();
+      //var getStatusPoll = function() {
+      //  this.getStatus;
+      //  setTimeout(getStatusPoll, 1000);
+      //};
+      //setTimeout(this.getStatus, 1000);
+    },
+  };
 </script>
