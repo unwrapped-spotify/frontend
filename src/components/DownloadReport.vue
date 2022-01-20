dcd61c6cae29e8d7ee9e8553ceb763c2c065cd2a
George Desktop <georgecaws@gmail.com>
2022-01-20 22:21:23 +0000

    Download page works now

--- src/components/DownloadReport.vue
+++ src/components/DownloadReport.vue
@@ -1,7 +1,26 @@
 <template>
-  <v-container>
-    <div class="text-center white--text">{{ this.status }}</div>
-    <div class="text-center white--text">{{ this.downloadLink }}</div>
+  <v-container fill-height fluid>
+    <v-row
+      justify="center"
+      class="text-center white--text"
+      align-content="center"
+    >
+      <v-col cols="2"></v-col>
+      <v-col cols="8">
+        <h1>Download your report</h1>
+        <br />
+        <p>{{ this.status }}</p>
+        <v-btn
+          rounded
+          :disabled="this.ready"
+          :color="$vuetify.theme.themes.dark.primary"
+          :href="this.downloadLink" 
+        >
+          Download
+        </v-btn>
+      </v-col>
+      <v-col cols="2"></v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -12,6 +31,7 @@
       return {
         downloadLink: "",
         status: "",
+        ready: true,
       };
     },
     methods: {
@@ -30,8 +50,20 @@
           .then((response) => {
             this.status = response.data.status;
             if (response.data.status == "SUCCESS") {
-              this.downloadLink = process.env.VUE_APP_BACKEND_URL + "/api/v1/report/" + this.storageID + "/download.html";
+              this.ready = false;
+              this.status = "Your report is ready to download!";
+              this.downloadLink =
+                process.env.VUE_APP_BACKEND_URL +
+                "/api/v1/report/" +
+                this.storageID +
+                "/download.html";
             } else {
+              if (response.data.status == "FAILED") {
+                this.status =
+                  "Your report failed to generate. Please try again.";
+              } else {
+                this.status = "Your report is being generated. Please wait.";
+              }
               setTimeout(this.getStatus, 1000);
             }
           });
